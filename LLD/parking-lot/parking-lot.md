f5444b4259269d8483e984bbe4581dff92ff5ff3
Purushothaman U <55243875+pksharma132@users.noreply.github.com>
2025-07-24 11:00:44 +0530

    md test

---
+++ LLD/parking-lot/parking-lot.md
@@ -0,0 +1,20 @@
+
+Design a parking lot system that supports:
+1. Multiple levels/floors
+2. Multiple types of parking spots (for cars, bikes, trucks)
+3. Parking and un-parking of vehicles
+4. Finding the nearest available spot
+5. Real-time availability tracking
+
+Functional Requirements
+1. A vehicle can enter and park in the nearest available spot.
+2. A vehicle can exit and free the spot
+3. Support multiple vehicle types: Car, Bike, Truck
+4. Support different spot sizes: Small, Medium, Large
+5. Ability to query available spots by type or floor
+
+Non-functional Requirements
+1. Thread-safety (in real-world, but can skip for now)
+2. Scalable design
+3. Clean object-oriented structure
+
---
+++ LLD/parking-lot/parking-lot.ts
@@ -0,0 +1,26 @@
+/**
+ *
+ * Design a parking lot system that supports:
+ *  - Multiple levels/floors
+ *  - Multiple types of parking spots (for cars, bikes, trucks)
+ *  - Parking and un-parking of vehicles
+ *  - Finding the nearest available spot
+ *  - Real-time availability tracking
+ * 
+ */
+
+/** Functional Requirements
+ *  - A vehicle can enter and park in the nearest available spot.
+ *  - A vehicle can exit and free the spot.
+ *  - Support multiple vehicle types: Car, Bike, Truck
+ *  - Support different spot sizes: Small, Medium, Large
+ *  - Ability to query available spots by type or floor
+ * 
+ */
+
+/** Non-functional Requirements 
+ *  - Thread-safety (in real-world, but can skip for now)
+ *  - Scalable design
+ *  - Clean object-oriented structure
+ * 
+*/
