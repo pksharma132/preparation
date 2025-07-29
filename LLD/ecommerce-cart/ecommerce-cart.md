93489d8c065aabd2b365e784d9e76dc447eae6ad
Purushothaman U <55243875+pksharma132@users.noreply.github.com>
2025-07-29 18:08:44 +0530

    ecommerce-cart

---
+++ LLD/ecommerce-cart/ecommerce-cart.md
@@ -0,0 +1,27 @@
+Question:
+    Design the backend system for a shopping cart used in an e-commerce platform.
+
+Functional Requirements:
+1. Users should be able to add / remove products from the cart and checkout
+2. Cart can have multiple products and should be able to support discounts application
+3. Support multiple product prices(unit wise)
+4. Cart should compute the total price after applying the discount
+
+Non-functional requirements:
+1. Scalable to millions of users
+2. Extensible to support multiple promotions
+3. support multiple sellers
+
+
+Core Entities:
+
+1. User - owner of the cart
+2. Product - different products
+3. Cart - will have user, discount and multiple CartItem
+4. CartItem - will have the product along with the price of the product at the time of adding to the cart
+5. Discount - can be of different types flat | amount
+
+
+
+
+
---
+++ LLD/ecommerce-cart/ecommerce-cart.ts
@@ -0,0 +1,75 @@
+class User {
+    constructor(readonly id: number, name: string, address: string[], currency: string) { }
+}
+
+
+class Product {
+    constructor(readonly id: number, readonly name: string, public unitPrice: number, readonly seller: string) { }
+}
+
+class CartItem {
+    constructor(public product: Product, public quantity: number) {
+    }
+
+    getCartItemPrice() {
+        return this.product.unitPrice * this.quantity
+    }
+}
+
+interface DiscountStrategy {
+    discountType: "flat" | "amount";
+    discountValue: number;
+    getDiscountedPrice(price: number): number;
+}
+
+class Flat implements DiscountStrategy {
+    discountType: "flat" = "flat";
+
+    constructor(readonly discountValue: number) {
+        this.discountValue = discountValue;
+    }
+    getDiscountedPrice(price: number): number {
+        return price - (price * (this.discountValue / 100))
+    }
+}
+
+class Amount implements DiscountStrategy {
+    discountType: "amount" = "amount";
+
+    constructor(readonly discountValue: number) {
+        this.discountValue = discountValue;
+    }
+    getDiscountedPrice(price: number): number {
+        return price - this.discountValue;
+    }
+}
+
+
+class Cart {
+    private discount: DiscountStrategy | null = null;
+    constructor(private cartItems: CartItem[]) { }
+    addCartItem(product: Product, quantity: number) {
+        this.cartItems.push(new CartItem(product, quantity));
+        return this.cartItems;
+    }
+
+    removeCartItem(product: Product) {
+        this.cartItems = this.cartItems.filter((item) => item.product.id !== product.id);
+        return this.cartItems;
+    }
+
+
+    getTotalPrice() {
+        const total = this.cartItems.reduce((prev, curr) => prev + curr.getCartItemPrice(), 0)
+        return total;
+    }
+
+    addDiscount(discount: DiscountStrategy) {
+        this.discount = discount;
+    }
+
+    getDiscountedPrice() {
+        const totalPrice = this.getTotalPrice();
+        return this.discount ? this.discount.getDiscountedPrice(totalPrice) : totalPrice;
+    }
+}
